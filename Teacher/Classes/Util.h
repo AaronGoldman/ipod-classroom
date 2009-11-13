61fc0c5745d09895226ab73eff5497f924987ce6
Smith.AdrianE <Smith.AdrianE@860c2de8-9c35-11de-8595-bbd473b9459f>
2009-11-13 19:15:22 +0000

    util added

---
+++ Teacher/Classes/Util.h
@@ -0,0 +1,28 @@
+//
+//  Util.h
+//  Addressbook
+//
+//  Created by Adrian Smith on 2/2/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CommonCrypto/CommonDigest.h>
+
+@interface Util : NSObject {
+
+}
+
++ (NSString*) md5:(NSString*)message;
++ (NSDictionary*) queryParameters:(NSURL*)url;
++ (NSString*) sanitizePhoneNumber:(NSString*)recipientPhone;
++ (NSString*) appDir;
++ (UIView*) viewFromNibNamed:(NSString*)nibname;
++ (UIImage*) resizeImage:(UIImage*)image newSize:(CGRect)newSize;
++ (UIColor*) appleBlueColor;
++ (NSString*) urlEncode:(NSString*)str;
++ (void) showAlertWithTitle:(NSString*)title message:(NSString*)message;
++ (BOOL) isPhone;
++ (NSString*) appVersion;
+
+@end
---
+++ Teacher/Classes/Util.m
@@ -0,0 +1,168 @@
+//
+//  Util.m
+//  Addressbook
+//
+//  Created by Adrian Smith on 2/2/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "Util.h"
+
+#include <sys/types.h>
+#include <sys/sysctl.h>
+
+@implementation Util
+
+
++ (NSString*) md5:(NSString*)message{
+	unsigned char result[CC_MD5_DIGEST_LENGTH];
+	
+	CC_MD5( [message UTF8String], [message lengthOfBytesUsingEncoding:NSUTF8StringEncoding], result );
+	
+	return [NSString 
+			stringWithFormat: @"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
+			result[0], result[1],
+			result[2], result[3],
+			result[4], result[5],
+			result[6], result[7],
+			result[8], result[9],
+			result[10], result[11],
+			result[12], result[13],
+			result[14], result[15]
+			];
+}
+
++ (NSDictionary*) queryParameters:(NSURL*)url{
+	NSArray* params = [[url query] componentsSeparatedByString:@"&"];
+	NSMutableDictionary* queryDict = [NSMutableDictionary dictionaryWithCapacity:[params count]];
+	
+	for(int i = 0; i < [params count]; i ++){
+		NSArray* parts = [[params objectAtIndex:i] componentsSeparatedByString:@"="];
+		[queryDict setObject:[parts objectAtIndex:1] forKey:[parts objectAtIndex:0]];
+	}
+	
+	for( NSString* key in queryDict){
+		NSLog(@"%@: %@" , key , [queryDict objectForKey:key]);
+	}
+	
+	return queryDict;
+}
+
++ (NSString*) sanitizePhoneNumber:(NSString*)recipientPhone{
+	recipientPhone = [recipientPhone stringByReplacingOccurrencesOfString:@"#" withString:@""];
+	recipientPhone = [recipientPhone stringByReplacingOccurrencesOfString:@"-" withString:@""];
+	recipientPhone = [recipientPhone stringByReplacingOccurrencesOfString:@"-" withString:@""];
+	recipientPhone = [recipientPhone stringByReplacingOccurrencesOfString:@"." withString:@""];
+	recipientPhone = [recipientPhone stringByReplacingOccurrencesOfString:@")" withString:@""];
+	recipientPhone = [recipientPhone stringByReplacingOccurrencesOfString:@"(" withString:@""];
+	recipientPhone = [recipientPhone stringByReplacingOccurrencesOfString:@" " withString:@""];
+	recipientPhone = [recipientPhone stringByReplacingOccurrencesOfString:@"" withString:@""];
+	return recipientPhone;
+}
+
++ (NSString*)appDir{
+	NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    if (!documentsDirectory) {
+        NSLog(@"Documents directory not found!");
+        return NO;
+    }
+	
+	return documentsDirectory;
+}
+
++ (UIView*) viewFromNibNamed:(NSString*)nibname{
+	UIView* view = nil;
+	
+	NSArray* tops = [[NSBundle mainBundle] loadNibNamed:nibname owner:nibname options:nil];
+	
+	for( int i = 0;i < [tops count];i ++){
+		if ( [[tops objectAtIndex:i] isKindOfClass:[UIView class]]){
+			view = [tops objectAtIndex:i];
+			break;
+		}
+	}	
+	
+	if( view == nil){
+		NSException* ex = [NSException exceptionWithName:@"Util: Bad nib Exception" 
+												  reason:[NSString stringWithFormat:@"Could not load a UIView from nib %@", nibname]
+												userInfo:nil];
+		[ex raise];
+	}
+	
+	return view;
+}
+
++ (UIImage*) resizeImage:(UIImage*)image newSize:(CGRect)newSize{
+	CGRect resizeFrame = newSize;
+	UIGraphicsBeginImageContext( resizeFrame.size );
+	[image drawInRect:resizeFrame];
+	image = UIGraphicsGetImageFromCurrentImageContext();
+	UIGraphicsEndImageContext();
+	
+	return image;
+}
+
+
++ (UIColor*) appleBlueColor{
+	return [UIColor colorWithRed:68.0/255 green:94.0f/255 blue:144.0f/255 alpha:1.0f];
+}
+
+
++ (NSString*) urlEncode:(NSString*)str{
+	str = [str stringByReplacingOccurrencesOfString:@" " withString:@"%20"];
+	str = [str stringByReplacingOccurrencesOfString:@"#" withString:@"%23"];
+	str = [str stringByReplacingOccurrencesOfString:@"$" withString:@"%24"];
+	str = [str stringByReplacingOccurrencesOfString:@"&" withString:@"%26"];
+	str = [str stringByReplacingOccurrencesOfString:@"+" withString:@"%2B"];
+	str = [str stringByReplacingOccurrencesOfString:@"," withString:@"%2C"];
+	str = [str stringByReplacingOccurrencesOfString:@"/" withString:@"%2F"];
+	str = [str stringByReplacingOccurrencesOfString:@":" withString:@"%3A"];
+	str = [str stringByReplacingOccurrencesOfString:@";" withString:@"%3B"];
+	str = [str stringByReplacingOccurrencesOfString:@"<" withString:@"%3C"];
+	str = [str stringByReplacingOccurrencesOfString:@"=" withString:@"%3D"];
+	str = [str stringByReplacingOccurrencesOfString:@">" withString:@"%3E"];
+	str = [str stringByReplacingOccurrencesOfString:@"?" withString:@"%3F"];
+	str = [str stringByReplacingOccurrencesOfString:@"@" withString:@"%40"];
+	str = [str stringByReplacingOccurrencesOfString:@"[" withString:@"%5B"];
+	str = [str stringByReplacingOccurrencesOfString:@"]" withString:@"%5D"];
+	str = [str stringByReplacingOccurrencesOfString:@"^" withString:@"%5E"];
+	str = [str stringByReplacingOccurrencesOfString:@"`" withString:@"%60"];
+	str = [str stringByReplacingOccurrencesOfString:@"{" withString:@"%7B"];
+	str = [str stringByReplacingOccurrencesOfString:@"|" withString:@"%7C"];
+	str = [str stringByReplacingOccurrencesOfString:@"}" withString:@"%7D"];
+	str = [str stringByReplacingOccurrencesOfString:@"~" withString:@"%7E"];
+	str = [str stringByReplacingOccurrencesOfString:@"\\" withString:@"%5C"];
+	
+	return str;
+}
+
++ (void) showAlertWithTitle:(NSString*)title message:(NSString*)message{
+	UIAlertView* av = [[UIAlertView alloc] initWithTitle:title
+												 message:message
+												delegate:nil
+									   cancelButtonTitle:nil
+									   otherButtonTitles:@"OK",nil];
+	[av show];
+	[av release];
+}
+
++ (NSString*) appVersion{
+	return [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"];
+}
+
++ (BOOL) isPhone{
+	int mib[2];
+	size_t len = 255;
+	char p[255];
+	
+	mib[0] = CTL_HW;
+	mib[1] = HW_MACHINE;
+	sysctl(mib, 2, p, &len, NULL, 0);
+	p[6] = 0;
+	
+	return !strncmp("iPhone", p, 6);
+
+}
+
+@end

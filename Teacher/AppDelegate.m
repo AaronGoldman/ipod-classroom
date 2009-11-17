6dc1e62ba627dccdca310ce65eadadef13f52d14
Smith.AdrianE <Smith.AdrianE@860c2de8-9c35-11de-8595-bbd473b9459f>
2009-11-17 20:53:11 +0000

    Added HTTP UI to project

--- Teacher/AppDelegate.m
+++ Teacher/AppDelegate.m
@@ -7,7 +7,8 @@
 //
 
 #import "AppDelegate.h"
-
+#import "DatabaseConnection.h"
+#import "Util.h"
 
 @implementation AppDelegate
 @synthesize window;
@@ -20,10 +21,39 @@
     [window addSubview:viewController.view];
     [window makeKeyAndVisible];
 	
+	//Create a server directory
+	NSString* toDirectory = [[Util appDir] stringByAppendingPathComponent:@"www"];
+	if( ![[NSFileManager defaultManager] fileExistsAtPath:toDirectory]){
+		[[NSFileManager defaultManager] createDirectoryAtPath:toDirectory attributes:nil];
+	}
+	//Copy all, html, js, and htm files to the server directory
+	NSMutableArray* pathsForServerFiles = [NSMutableArray arrayWithCapacity:50];
+	[pathsForServerFiles addObjectsFromArray:[[NSBundle mainBundle] pathsForResourcesOfType:@"html" inDirectory:nil]];
+	[pathsForServerFiles addObjectsFromArray:[[NSBundle mainBundle] pathsForResourcesOfType:@"js" inDirectory:nil]];
+	[pathsForServerFiles addObjectsFromArray:[[NSBundle mainBundle] pathsForResourcesOfType:@"htm" inDirectory:nil]];
+	NSError* error = nil;
+	for ( NSString* path in pathsForServerFiles){
+		NSString* filename = [path lastPathComponent];
+		NSString* toPath = [toDirectory stringByAppendingPathComponent:filename];
+		[[NSFileManager defaultManager] copyItemAtPath:path toPath:toPath  error:&error];
+		if ( error){
+			NSLog(@"error copying server file <%@>: %@" ,filename, error );
+			error = nil;
+		}
+	}
+
 	
+//[DatabaseConnection executeSelect:@"INSERT INTO class (name, class_id) VALUES ('Tim', NULL);"];
+	[DatabaseConnection executeSelect:@"DELETE FROM class WHERE class_id>3;"];
+		NSArray* vals = [DatabaseConnection executeSelect:@"SELECT * FROM class WHERE class_id>0"];
 	
-	//id object;
-	//NSLog(@"object name: %@",object);
+	NSLog(@"vals: %@", vals);
+	NSLog(@"val: %@" , [vals objectAtIndex:1]);
+	int thirdRowVal = [[[vals objectAtIndex:1] objectForKey:@"class_id"] intValue];
+	
+	id object;
+	NSLog(@"object name: %@",vals);
+	NSLog(@"Row: %d",thirdRowVal);
 }
 
 
---
+++ Teacher/Classes/HTTPViewController.m
@@ -0,0 +1,149 @@
+//
+//  HTTPViewController.m
+//  Teacher
+//
+//  Created by ece on 11/17/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "HTTPViewController.h"
+#import "HTTPServer.h"
+#import "MyHTTPConnection.h"
+#import "localhostAddresses.h"
+#import "Util.h"
+
+@implementation HTTPViewController
+
+/*
+ // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
+        // Custom initialization
+    }
+    return self;
+}
+*/
+
+/*
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+*/
+
+/*
+// Override to allow orientations other than the default portrait orientation.
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+*/
+
+
+- (void)viewDidLoad
+{
+
+	
+	NSString *root = [[Util appDir] stringByAppendingPathComponent:@"www"];
+	
+	httpServer = [HTTPServer new];
+	[httpServer setType:@"_http._tcp."];
+	[httpServer setConnectionClass:[MyHTTPConnection class]];
+	[httpServer setDocumentRoot:[NSURL fileURLWithPath:root]];
+	
+	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(displayInfoUpdate:) name:@"LocalhostAdressesResolved" object:nil];
+	[localhostAddresses performSelectorInBackground:@selector(list) withObject:nil];
+}
+
+- (void)displayInfoUpdate:(NSNotification *) notification
+{
+	NSLog(@"displayInfoUpdate:");
+	
+	if(notification)
+	{
+		[addresses release];
+		addresses = [[notification object] copy];
+		NSLog(@"addresses: %@", addresses);
+	}
+	
+	if(addresses == nil)
+	{
+		return;
+	}
+	
+	NSString *info;
+	UInt16 port = [httpServer port];
+	
+	NSString *localIP = nil;
+	
+	localIP = [addresses objectForKey:@"en0"];
+	
+	if (!localIP)
+	{
+		localIP = [addresses objectForKey:@"en1"];
+	}
+	
+	if (!localIP)
+		info = @"Wifi: No Connection!\n";
+	else
+		info = [NSString stringWithFormat:@"http://iphone.local:%d		http://%@:%d\n", port, localIP, port];
+	
+	NSString *wwwIP = [addresses objectForKey:@"www"];
+	
+	if (wwwIP)
+		info = [info stringByAppendingFormat:@"Web: %@:%d\n", wwwIP, port];
+	else
+		info = [info stringByAppendingString:@"Web: Unable to determine external IP\n"];
+	
+	displayInfo.text = info;
+}
+
+
+- (IBAction)startStopServer:(id)sender
+{
+	if ([sender isOn])
+	{
+		// You may OPTIONALLY set a port for the server to run on.
+		// 
+		// If you don't set a port, the HTTP server will allow the OS to automatically pick an available port,
+		// which avoids the potential problem of port conflicts. Allowing the OS server to automatically pick
+		// an available port is probably the best way to do it if using Bonjour, since with Bonjour you can
+		// automatically discover services, and the ports they are running on.
+		[httpServer setPort:8080];
+		
+		NSError *error = nil;
+		if(![httpServer start:&error])
+		{
+			NSLog(@"Error starting HTTP Server: %@", error);
+		}
+		
+		[self displayInfoUpdate:nil];
+	}
+	else
+	{
+		[httpServer stop];
+	}
+}
+
+
+
+- (void)didReceiveMemoryWarning {
+	// Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+	
+	// Release any cached data, images, etc that aren't in use.
+}
+
+- (void)viewDidUnload {
+	// Release any retained subviews of the main view.
+	// e.g. self.myOutlet = nil;
+}
+
+
+- (void)dealloc {
+	[httpServer release];
+    [super dealloc];
+}
+
+
+@end

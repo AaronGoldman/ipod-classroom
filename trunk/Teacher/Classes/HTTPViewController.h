1f859fbc27cee724cc2ad9e62aee019a960bbb97
Smith.AdrianE <Smith.AdrianE@860c2de8-9c35-11de-8595-bbd473b9459f>
2009-11-17 20:53:11 +0000

    Added HTTP UI to project

---
+++ trunk/Teacher/Classes/HTTPViewController.h
@@ -0,0 +1,21 @@
+//
+//  HTTPViewController.h
+//  Teacher
+//
+//  Created by ece on 11/17/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class HTTPServer;
+
+
+@interface HTTPViewController : UIViewController {
+	IBOutlet UILabel* displayInfo; 
+	HTTPServer *httpServer;
+	NSDictionary *addresses;
+}
+
+-(IBAction) startStopServer:(id)sender;
+
+@end
